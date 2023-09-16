c61cc94ee2b0391be0cf4620efb93077b8aca213
Escalloniales <cyguchi@gmail.com>
2023-09-16 19:56:26 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 /**
  * Solve the FizzBuzz challenge.
  */
@@ -7,28 +9,64 @@ class FizzBuzz {
 
         for (int i = 1; i < 100; i++) {
 
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+            doFizzBuzz(i);
+        }
+    }
 
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
+    private static void doFizzBuzz(int i) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(i);
+
+        }
+    }
+}
+
+class FizzBuzz2 {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i < 100) {
+            boolean d3 = i % 3 == 0;
+            boolean d5 = i % 5 == 0;
+
+            if (d3 && d5) {
 
                 System.out.println("Fizz Buzz");
 
-            } else if (divisibleBy3) {
+            } else if (d3) {
 
                 System.out.println("Fizz");
 
-            } else if (divisibleBy5) {
+            } else if (d5) {
 
                 System.out.println("Buzz");
 
             } else {
 
                 System.out.println(i);
-
             }
+            
+            i++;
+
         }
     }
 }
